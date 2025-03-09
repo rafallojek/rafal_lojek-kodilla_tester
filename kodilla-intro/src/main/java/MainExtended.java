2bbdfa54a47591951d4a171ae9adf018bc1e5e7c
Rafal Lojek <rafi8820@gmail.com>
2025-03-09 10:55:15 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,25 @@
+package com.kodilla;
+
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Rafal";
+        int myAge = 37;
+        double height = 1.75;
+        char carClass = 'A';
+        boolean isGoodProgrammer = false;
+        String myText = "Czesc, jestem nowym kursantem szkoly Kodilla i przyszlym Testerem Automatyzujacym";
+        //0.8.Zmienne. Zadanie: deklarowanie i wywoływanie zmiennych.
+        //Zadeklarować 6 własnych zmiennych o dowolnych, poznanych mi typach
+        //oraz wywołać każdą z nich, aby jej wartość była widoczna w konsoli.
+        //Gotowy kod umieszczony został w repozytorium.
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+        System.out.println(myText);
+        //Wywołanie wartości poprzez użycie funkcji System.out.println();,
+        //dzięki czemu te wartości są dla Nas widoczne/czytelne.
+    }
+}
--- kodilla-intro/src/main/java/com/kodilla/MainExtended.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla;
-
-public class MainExtended {
-    public static void main(String[] args) {
-
-    }
-}
