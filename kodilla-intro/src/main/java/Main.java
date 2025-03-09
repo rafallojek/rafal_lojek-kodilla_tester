0e014b92931e27d274725b5e7dea9d88257d9115
Rafal Lojek <rafi8820@gmail.com>
2025-03-09 08:24:17 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        //Komenda System.out.println(myName);
+        //Komenda wyżej, aby konsola wyświetliła nam zadeklarowane wyżej wartości.
+        //Po naciśnięciu przycisku RUN, ta wartość została wyświetlona.
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+        //Przetestowałem tę samę komendę jak wyżej dla pozostałych wartości.
+        //Zadziałała. Wyświetliła to czego zażądałem.
+    }
+}
