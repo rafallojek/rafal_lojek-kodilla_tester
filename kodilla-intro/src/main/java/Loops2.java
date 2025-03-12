3a03d4143e8575a715efb237844d9c4cf5fd31bc
Rafal Lojek <rafi8820@gmail.com>
2025-03-12 19:52:04 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/Loops2.java
@@ -0,0 +1,10 @@
+public class Loops2 {
+    public static void main(String[] args) {
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+        for (int i = names.length - 1; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+    }
+}
+//Tutaj też wkradł mi się chochlik, bo za szybko chciałem napisać kod. Również szybko błąd wyłapany.
+//Cwiczenie jak w Loops1, z tym że wyświetlanie elementów tablicy od końca.
