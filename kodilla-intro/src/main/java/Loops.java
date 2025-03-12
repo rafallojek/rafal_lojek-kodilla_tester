3098d54b3060d7d94f2192dc953e7a0853d7e5b4
Rafal Lojek <rafi8820@gmail.com>
2025-03-12 19:34:49 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//Tutaj w tym ćwiczeniu miałem 2 errory. Szybko doszedłem o co chodzi.
+//W 1 przypadku zgubiłem jeden }, w 2 przypadku zgubiłem (String po słowie main.
+//Fajna zabawa, nie odpuszczam. Szukam, klikam, czytam, przeglądam kod :)
+
+//Odnośnie tego ćwiczenia, czy tutaj nie ma klas równoważności i wartości brzegowych?
+
