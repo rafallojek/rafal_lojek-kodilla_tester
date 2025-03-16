5833f192a2054f03796c04e97648d341e715c2ff
Rafal Lojek <rafi8820@gmail.com>
2025-03-16 20:21:12 +0100

    Initial version

--- kodilla-intro/src/main/java/com/kodilla/Notebook.java
+++ kodilla-intro/src/main/java/com/kodilla/Notebook.java
@@ -2,7 +2,8 @@ package com.kodilla;
 
 public class Notebook {
     public static void main(String[] args) {
-
+        int weight = 600;
+        Notebook notebook = new Notebook();
+        System.out.println(weight);
     }
 }
-//Ćwiczenie w trakcie...
