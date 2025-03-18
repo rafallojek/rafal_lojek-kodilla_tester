7e9fecf10da3579ac89314e53db552279fab3e48
Rafal Lojek <rafi8820@gmail.com>
2025-03-18 07:54:01 +0100

    Initial version

--- kodilla-intro/src/main/java/com/kodilla/FirstClass.java
+++ kodilla-intro/src/main/java/com/kodilla/FirstClass.java
@@ -1,13 +1,6 @@
 package com.kodilla;
 
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from com.kodilla.FirstClass!");// [3]
-
-        if (1 > 2){
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niz dwa.");
-        }
+public class FirstClass {
+    public static void main(String[] args) {
     }
 }
