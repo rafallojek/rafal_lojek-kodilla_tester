901684049932b0f89fdcfb069058d68e38347340
Rafal Lojek <rafi8820@gmail.com>
2025-03-23 15:18:12 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/com/kodilla/User.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class User {
+   public static void main(String[] args) {
+       String myName = "Rafal";
+       int myAge = 37;
+
+       System.out.println(myName);
+       System.out.println(myAge);
+   }
+}
+
