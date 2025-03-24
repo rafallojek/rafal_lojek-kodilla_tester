38e3460ed5bb63901bc18703772e2f5d17a02f02
Rafal Lojek <rafi8820@gmail.com>
2025-03-24 19:36:24 +0100

    Initial version

--- kodilla-intro/src/main/java/com/kodilla/AdvCalcApplication.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla;
-
-public class AdvCalcApplication {
-    public static void main(String[] args) {
-        AdvCalculator calculator = new AdvCalculator();
-        double result = calculator.calculate();
-        System.out.println("Result: " + result);
-    }
-}
