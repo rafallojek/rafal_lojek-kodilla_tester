a623975f63222932b280f87208bf941cf188dd2f
Rafal Lojek <rafi8820@gmail.com>
2025-04-28 22:55:56 +0200

    Initial version

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/Random.java
@@ -0,0 +1,17 @@
+package com.kodilla.collections.arrays;
+
+public class Random {
+    Random random = new Random();
+    private int maxValue;
+    private int minValue;
+    int drawnNumber = random.nextInt(160+1 - 100) + 100;
+
+    int nextInt(int i) {
+    }
+
+    public int nextDouble() {
+    }
+
+    Random random = new Random();
+    Shape[] shapes = new Shape[random.nextInt(20) + 1];
+}
