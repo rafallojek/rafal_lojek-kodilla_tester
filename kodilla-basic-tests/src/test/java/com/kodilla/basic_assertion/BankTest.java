f434770937950b72319f4f816a9e1f4e5584bf56
Rafal Lojek <rafi8820@gmail.com>
2025-04-28 22:32:55 +0200

    Initial version

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/BankTest.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/BankTest.java
@@ -11,53 +11,81 @@ public class BankTest {
 
     @BeforeEach
     public void setUp() {
-        bank = new Bank(2);  // Tworzymy bank z dwoma bankomatami
+        bank = new Bank(2);
     }
 
     @Test
     public void testTotalBalance() {
+        // Given
         bank.cashMachines[0].addTransaction(100);
         bank.cashMachines[0].addTransaction(-50);
         bank.cashMachines[1].addTransaction(200);
         bank.cashMachines[1].addTransaction(-100);
 
-        assertEquals(150, bank.getTotalBalance(), 0.001);
+        // When
+        double totalBalance = bank.getTotalBalance();
+
+        // Then
+        assertEquals(150, totalBalance, 0.001);
     }
 
     @Test
     public void testTotalWithdrawals() {
+        // Given
         bank.cashMachines[0].addTransaction(-50);
         bank.cashMachines[1].addTransaction(-100);
 
-        assertEquals(2, bank.getTotalWithdrawalCount());
+        // When
+        int totalWithdrawals = bank.getTotalWithdrawalCount();
+
+        // Then
+        assertEquals(2, totalWithdrawals);
     }
 
     @Test
     public void testTotalDeposits() {
+        // Given
         bank.cashMachines[0].addTransaction(100);
         bank.cashMachines[1].addTransaction(200);
 
-        assertEquals(2, bank.getTotalDepositCount());
+        // When
+        int totalDeposits = bank.getTotalDepositCount();
+
+        // Then
+        assertEquals(2, totalDeposits);
     }
 
     @Test
     public void testAverageWithdrawal() {
+        // Given
         bank.cashMachines[0].addTransaction(-50);
         bank.cashMachines[1].addTransaction(-100);
 
-        assertEquals(-75, bank.getAverageWithdrawal(), 0.001);
+        // When
+        double averageWithdrawal = bank.getAverageWithdrawal();
+
+        // Then
+        assertEquals(-75, averageWithdrawal, 0.001);
     }
 
     @Test
     public void testAverageDeposit() {
+        // Given
         bank.cashMachines[0].addTransaction(100);
         bank.cashMachines[1].addTransaction(200);
 
-        assertEquals(150, bank.getAverageDeposit(), 0.001);
+        // When
+        double averageDeposit = bank.getAverageDeposit();
+
+        // Then
+        assertEquals(150, averageDeposit, 0.001);
     }
 
     @Test
     public void testEmptyBank() {
+        // Given
+
+        // When & Then
         assertEquals(0, bank.getTotalBalance(), 0.001);
         assertEquals(0, bank.getTotalWithdrawalCount());
         assertEquals(0, bank.getTotalDepositCount());
