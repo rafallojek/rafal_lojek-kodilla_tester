a43649fe306e04950216cf9f948403e785b4cbd8
Rafal Lojek <rafi8820@gmail.com>
2025-04-29 21:29:50 +0200

    Initial version

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,31 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return (double) a / b;
+    }
+
+    public double power(int a) {
+        return a * a;
+    }
+
+    public double incorrectPower(int a) {
+        return a + a;
+    }
+}
