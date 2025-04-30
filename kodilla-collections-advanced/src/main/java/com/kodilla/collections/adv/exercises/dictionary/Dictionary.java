f4ec2910c0e9af755fe86e9c0abd465e94ee3c06
Rafal Lojek <rafi8820@gmail.com>
2025-05-01 01:32:47 +0200

    Initial version

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictionary/Dictionary.java
@@ -0,0 +1,22 @@
+package com.kodilla.collections.adv.exercises.dictionary;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Dictionary {
+    Map<String, List<EnglishWord>> dictionary = new HashMap<>();
+    
+    public void addWord(String polishWord, EnglishWord englishWord) {
+        
+    }
+    
+    public List<EnglishWord> findEnglishWords(String polishWord) {
+        return Collection.emptyList();
+    }
+    
+    public List<EnglishWord> findEnglishWords(String polishWord, PartOfSpeech partOfSpeech) {
+        return Collection.emptyList();
+    }
+}
