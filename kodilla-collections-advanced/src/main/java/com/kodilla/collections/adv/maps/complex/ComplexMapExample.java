53795ca361c275027859c9c83ffc13e697b074d2
Rafal Lojek <rafi8820@gmail.com>
2025-04-30 22:42:03 +0200

    Initial version

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/ComplexMapExample.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.maps.complex;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComplexMapExample {
+    public static void main(String[] args) {
+        Map<Student, Grades> school = new HashMap<>();
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/Grades.java
@@ -0,0 +1,12 @@
+package com.kodilla.collections.adv.maps.complex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grades {
+    private List<Double> grades = new ArrayList<>();
+
+    public Grades(List<Double> grades) {
+        this.grades = grades;
+    }
+}
