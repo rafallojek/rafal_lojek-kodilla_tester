bbbe405ac6918a39132d84ce3bc51cf0071b0034
Rafal Lojek <rafi8820@gmail.com>
2025-05-01 08:42:03 +0200

    Initial version

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/dictionary/DictionaryTestSuite.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/dictionary/DictionaryTestSuite.java
@@ -2,6 +2,8 @@ package com.kodilla.collections.adv.exercises.dictionary;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryTestSuite {
@@ -16,5 +18,19 @@ class DictionaryTestSuite {
         dictionary.addWord(polishWord, englishWord);
         // Then
         assertEquals(1, dictionary.size());
+    }
+
+    @Test
+    public void testFindEnglishWords(){
+        // Given
+        Dictionary dictionary = new Dictionary();
+        dictionary.addWord("stos", new EnglishWord(PartOfSpeech.NOUN, "stack"));
+        dictionary.addWord("gra", new EnglishWord(PartOfSpeech.NOUN, "play"));
+        dictionary.addWord("gra", new EnglishWord(PartOfSpeech.NOUN, "game"));
+        dictionary.addWord("grac", new EnglishWord(PartOfSpeech.NOUN, "play"));
+        // When
+        List<EnglishWord> result = dictionary.findEnglishWords("gra");
+        // Then
+        assertEquals(2, result.size());
     }
 }
