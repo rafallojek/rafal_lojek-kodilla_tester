648e21c398ef977995dd658fbb68ecb4c9ec7c13
Rafal Lojek <rafi8820@gmail.com>
2025-05-03 23:11:23 +0200

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,14 +1,25 @@
 package com.kodilla.stream;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsersManager {
     public static void main(String[] args) {
-        processUsersStream();
+        List<String> chemistGroupUsernames = filterChemistGroupUsernames();
+        System.out.println(chemistGroupUsernames);
     }
 
-    private static void processUsersStream() {
-        UsersRepository.getUsersList()
+    private static List<String> filterChemistGroupUsernames() {
+        List<String> usernames = UsersRepository.getUsersList()   // [1]
                 .stream()
-                .map(user -> user.getUsername())
-                .forEach(username -> System.out.println(username));
+                .filter(user -> user.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());                      // [2]
+
+        return usernames;
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
     }
 }
