d063a494ece3022ab704b2946913a0597c010bca
Rafal Lojek <rafi8820@gmail.com>
2025-05-03 22:02:09 +0200

    Initial version

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -30,8 +30,8 @@ public class FlightRepository {
 
         flights.add(new Flight("Paris", "Hamburg"));
         flights.add(new Flight("Paris", "Toronto"));
-        flights.add(new Flight("Paris", "Warsaw"));
+        flights.add(new Flight("Paris", "Lublin"));
 
-        return flights;
+    return flights;
     }
 }
