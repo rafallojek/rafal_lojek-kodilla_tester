b5fb7f19bb72bde40f05f6a3b35c68ad1e367b52
Rafal Lojek <rafi8820@gmail.com>
2025-05-18 21:10:01 +0200

    Initial version

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
@@ -1,4 +1,43 @@
 package com.kodilla.execution_model.homework;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Order {
+    private final int value;
+    private final LocalDate date;
+    private final String login;
+
+    public Order(int value, LocalDate date, String login) {
+        this.value = value;
+        this.date = date;
+        this.login = login;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return value == order.value &&
+                Objects.equals(date, order.date) &&
+                Objects.equals(login, order.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, date, login);
+    }
 }
--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -1,4 +1,35 @@
 package com.kodilla.execution_model.homework;
 
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Shop {
+    private final Set<Order> orders = new HashSet<>();
+
+    public boolean addOrder(Order order) {
+        return orders.add(order);
+    }
+
+    public List<Order> getOrdersByDateRange(LocalDate startDate, LocalDate endDate) {
+        return orders.stream()
+                .filter(o -> !o.getDate().isBefore(startDate) && !o.getDate().isAfter(endDate))
+                .collect(Collectors.toList());
+    }
+
+    public List<Order> getOrdersByValueRange(int minValue, int maxValue) {
+        return orders.stream()
+                .filter(o -> o.getValue() >= minValue && o.getValue() <= maxValue)
+                .collect(Collectors.toList());
+    }
+
+    public int getOrderCount() {
+        return orders.size();
+    }
+
+    public int getTotalOrderValue() {
+        return orders.stream()
+                .mapToInt(Order::getValue)
+                .sum();
+    }
 }
