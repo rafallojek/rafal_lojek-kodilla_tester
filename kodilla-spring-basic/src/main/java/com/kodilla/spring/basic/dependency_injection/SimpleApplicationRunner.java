1f766c3308937c57bd4b7c17acfd8deae28a4e02
Rafal Lojek <rafi8820@gmail.com>
2025-05-20 23:21:15 +0200

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplicationRunner.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplicationRunner.java
@@ -3,7 +3,7 @@ package com.kodilla.spring.basic.dependency_injection;
 public class SimpleApplicationRunner {
 
     public static void main(String[] args) {
-        MessageService messageService = new MessageService();
+        MessageService messageService = new FacebookMessageService();
         SimpleApplication application = new SimpleApplication(messageService);
         application.processMessage("Test message", "receiver@mail.com");
     }
