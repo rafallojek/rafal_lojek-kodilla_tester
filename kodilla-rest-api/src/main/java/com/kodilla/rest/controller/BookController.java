8963fc871dd8ba07076728ce65613b79b868c561
Rafal Lojek <rafi8820@gmail.com>
2025-06-15 10:54:05 +0200

    Initial version

---
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<BookDto> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @PostMapping                                         // [1]
+    public void addBook(@RequestBody BookDto bookDto) {  // [2]
+        bookService.addBook(bookDto);
+    }
+}
