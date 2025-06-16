75007e7e07d5f6f5fc52f5ee37ae7116b4791a79
Rafal Lojek <rafi8820@gmail.com>
2025-06-16 21:30:07 +0200

    Initial version

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest2.java
@@ -0,0 +1,30 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.controller.BookController;
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.*;
+
+class BookControllerTest2 {
+
+    @Test
+    void shouldAddBook() {
+        // given
+        BookService bookService = mock(BookService.class);
+        BookController bookController = new BookController(bookService);
+        BookDto bookDto = new BookDto("Tytuł", "Autor");
+
+        // when
+        bookController.addBook(bookDto);
+
+        // then
+        ArgumentCaptor<BookDto> captor = ArgumentCaptor.forClass(BookDto.class);
+        verify(bookService, times(1)).addBook(captor.capture());
+        BookDto capturedBook = captor.getValue();
+        assert capturedBook.getTitle().equals("Tytuł");
+        assert capturedBook.getAuthor().equals("Autor");
+    }
+}
