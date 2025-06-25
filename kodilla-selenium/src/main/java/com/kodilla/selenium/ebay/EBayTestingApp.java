8012aadbf597e6aed250c44ea4f6f615e121cd40
Rafal Lojek <rafi8820@gmail.com>
2025-06-25 19:43:35 +0200

    Initial version

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -0,0 +1,30 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EBayTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/Selenium/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchField = driver.findElement(By.name("_nkw"));
+
+        searchField.sendKeys("Laptop");
+
+        searchField.submit();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
