641c6c57bb5455a8d77cd47b96f468276a959ad4
Rafal Lojek <rafi8820@gmail.com>
2025-06-25 22:45:53 +0200

    Initial version

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -2,43 +2,52 @@ package com.kodilla.selenium.allegro;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class AllegroTestingApp {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
         driver.manage().window().maximize();
 
-        Thread.sleep(3000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         try {
-            WebElement acceptCookies = driver.findElement(By.xpath("//button[contains(text(),'Przejdź do serwisu')]"));
+            WebElement acceptCookies = driver.findElement(By.cssSelector("button[data-role='accept-consent']"));
             acceptCookies.click();
-        } catch (Exception e) {
-
-        }
+        } catch (Exception ignored) {}
 
         try {
             Alert alert = driver.switchTo().alert();
             alert.accept();
-        } catch (NoAlertPresentException e) {
+        } catch (NoAlertPresentException ignored) {}
 
-        }
-
-        WebElement categoryDropdown = driver.findElement(By.xpath("//select[@data-role='filters-dropdown']"));
+        WebElement categoryDropdown = driver.findElement(By.cssSelector("select[data-role='filters-dropdown']"));
         categoryDropdown.click();
-        WebElement electronicsOption = driver.findElement(By.xpath("//option[text()='Elektronika']"));
+
+        WebElement electronicsOption = driver.findElement(By.cssSelector("option[value='/kategoria/elektronika']"));
         electronicsOption.click();
 
-        WebElement searchInput = driver.findElement(By.xpath("//input[@data-role='search-input']"));
+        WebElement searchInput = driver.findElement(By.cssSelector("input[data-role='search-input']"));
         searchInput.sendKeys("Mavic mini");
 
-        WebElement searchButton = driver.findElement(By.xpath("//button[@data-role='search-button']"));
+        WebElement searchButton = driver.findElement(By.cssSelector("button[data-role='search-button']"));
         searchButton.click();
 
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("section>article")));
+
+        List<WebElement> productCards = driver.findElements(By.cssSelector("section > article"));
+
+        for (WebElement product : productCards) {
+            System.out.println("Produkt:\n" + product.getText());
+            System.out.println("---------------");
+        }
 
         driver.quit();
     }
