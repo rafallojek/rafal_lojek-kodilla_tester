cc2bebcf61c55277870e31665eb7de6cb8650a9b
Rafal Lojek <rafi8820@gmail.com>
2025-06-28 11:29:26 +0200

    Initial version

---
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -0,0 +1,26 @@
+package wallet;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class WalletSteps {
+    private final Wallet wallet = new Wallet();
+    private final CashSlot cashSlot = new CashSlot();
+
+    @Given("I have deposited $200 in my wallet")
+    public void i_have_deposited_$200_in_my_wallet() {
+        wallet.deposit(200);
+        Assert.assertEquals("Incorrect wallet balance", 200, wallet.getBalance());
+    }
+    @When("I request $30")
+    public void i_request_$30() {
+        Cashier cashier = new Cashier(cashSlot);
+        cashier.withdraw(wallet, 30);
+    }
+    @Then("$30 should be dispensed")
+    public void $30_should_be_dispensed() {
+        Assert.assertEquals(30, cashSlot.getContents());
+    }
+}
