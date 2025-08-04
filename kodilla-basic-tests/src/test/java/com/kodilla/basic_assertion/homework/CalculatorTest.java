5fa86ba747ae68a911aab2c4851302637368b574
Rafal Lojek <rafi8820@gmail.com>
2025-08-04 22:03:36 +0200

    Initial version

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/homework/CalculatorTest.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/homework/CalculatorTest.java
@@ -1,5 +1,3 @@
-package com.kodilla.basic_assertion;
-
 import com.kodilla.basic_assertion.homework.Calculator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
