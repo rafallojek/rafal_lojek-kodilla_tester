f3f692a9cfbb0c385fb755a001a17bb478879925
Rafal Lojek <rafi8820@gmail.com>
2025-08-05 18:32:03 +0200

    Initial version

--- kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/ApplicationTest.java
+++ kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/ApplicationTest.java
@@ -1,31 +1,34 @@
 package com.kodilla.testcontainers;
 
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-import org.testcontainers.containers.DefaultRecordingFileFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ApplicationTest {
 
-    @Rule
-    public Network network = Network.newNetwork();
+    private final Network network = Network.newNetwork();
 
-    @Rule
-    public GenericContainer<?> webServer =
+    @Container
+    private final GenericContainer<?> webServer =
             new GenericContainer<>(
                     new ImageFromDockerfile()
                             .withFileFromClasspath("/tmp/index.html", "index.html")
@@ -38,26 +41,27 @@ public class ApplicationTest {
                     .withNetworkAliases("my-server")
                     .withExposedPorts(80);
 
-    @Rule
-    public BrowserWebDriverContainer<?> chrome =
+    @Container
+    private final BrowserWebDriverContainer<?> chrome =
             new BrowserWebDriverContainer<>()
                     .withNetwork(network)
                     .withRecordingMode(RECORD_ALL, new File("./build/"))
-                    .withRecordingFileFactory(new DefaultRecordingFileFactory())
                     .withCapabilities(new ChromeOptions());
 
-    @Test
-    public void customImageTest() throws InterruptedException, IOException {
-        // given
-        RemoteWebDriver driver = chrome.getWebDriver();
+    @BeforeAll
+    void createDirs() {
+        new File("./build/screenshots").mkdirs();
+    }
 
-        // when
+    @Test
+    void customImageTest() throws IOException {
+        RemoteWebDriver driver = chrome.getWebDriver();
         driver.get("http://my-server/");
+
         File screenshot = driver.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshot, new File("./build/screenshots/" + screenshot.getName()));
-        String title = driver.findElement(By.id("title")).getText();
 
-        // then
+        String title = driver.findElement(By.id("title")).getText();
         assertEquals("My dockerized web page.", title);
     }
 }
--- kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/BrowserTest.java
+++ kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/BrowserTest.java
@@ -1,42 +1,38 @@
 package com.kodilla.testcontainers;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 
-public class BrowserTest {
+@Testcontainers
+class BrowserTest {
 
-    private BrowserWebDriverContainer<?> chromeContainer;
+    @Container
+    private final BrowserWebDriverContainer<?> chromeContainer =
+            new BrowserWebDriverContainer<>()
+                    .withCapabilities(new ChromeOptions());
 
-    @BeforeEach
-    public void setUp() {
-        chromeContainer = new BrowserWebDriverContainer<>()
-                .withCapabilities(new ChromeOptions());
-        chromeContainer.start();
+    @BeforeAll
+    static void createDirs() {
+        new File("./build/screenshots").mkdirs();
     }
 
     @Test
-    public void chromeTest() throws IOException {
+    void chromeTest() throws IOException {
         RemoteWebDriver driver = chromeContainer.getWebDriver();
         driver.get("http://allegro.pl");
 
         File screenshot = driver.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshot,
                 new File("./build/screenshots/" + screenshot.getName()));
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if (chromeContainer != null) {
-            chromeContainer.stop();
-        }
     }
 }
